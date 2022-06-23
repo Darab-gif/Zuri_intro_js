ff6dcd2c534b46e71f52c6a7b5dd2f892bedcd92
Darab <darabcode@gmail.com>
2022-06-23 06:53:52 +0100

    Zuri intro to JavaScript

---
+++ script.js
@@ -0,0 +1,16 @@
+var name = "Darab";
+var height = "5.2ft";
+var country = "Nigeria";
+
+document.getElementById("demo").innerHTML =
+  "Name:" +
+  " " +
+  name +
+  "<br/>" +
+  "Height:" +
+  " " +
+  height +
+  "<br/>" +
+  "Country:" +
+  " " +
+  country;
